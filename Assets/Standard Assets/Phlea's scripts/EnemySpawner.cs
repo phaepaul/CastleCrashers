e8faf6a6af416f8b58afe518cc605226d078de6e
Phaedra Paul <phaedra@phaedra.net>
2018-10-31 17:29:27 -0400

    initial commit

---
+++ Assets/Standard Assets/Phlea's scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemySpawner : MonoBehaviour {
+
+	void Start () {
+		Scene season = SceneManager.GetActiveScene ();
+		switch(season) {
+		case "fall":
+			break;
+		case "spring":
+			break;
+		case "summer":
+			break;
+		case "winter":
+			break;
+		case "boss":
+			break;
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
